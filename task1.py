4682fb14f38230049dfbfbc7763e8947e7cd12a9
Stanislau Smeyanovich <ssmeyanovich@gmail.com>
2021-09-23 19:17:35 +0300

    task-1: resolving the task

---
+++ task1.py
@@ -0,0 +1,12 @@
+product = input('Insert the product:\n')
+ 
+if product == 'milk':
+    print(3)
+else:
+    if product == 'butter':
+        print(5)
+    else:
+        if product == 'bread':
+            print(7)
+        else:
+            print(-1)
